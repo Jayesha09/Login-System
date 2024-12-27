1e2d4bfa58dd88b714b321fbbb3affcdeeb6c2d4
Hades181 <dishnadb@gmail.com>
2024-12-27 16:19:27 +0400

    Update main.dart
    removed url and anonKey supabase

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,9 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {
   //supabase setup
   await Supabase.initialize(
-      url: "https://ljfzbjjbrlwnrmgitbqp.supabase.co",
+      url: "SUPABASE_URL",
       anonKey:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqZnpiampicmx3bnJtZ2l0YnFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMzNDIxNTYsImV4cCI6MjA0ODkxODE1Nn0.PcnJ7slS1JC--5Y4W7Xnl7vozD4SFc-xJLeKz7_tTKw");
+          "SUPABASE_KEY");
   runApp(const MyApp());
 }
 
